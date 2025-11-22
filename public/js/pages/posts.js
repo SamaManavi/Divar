64253494a1d6a0ab6b0c7b03f27fe61d51fae577
sama manavi <samamanavi1881@gmail.com>
2025-11-22 10:52:52 +0330

    develop note

--- public/js/pages/posts.js
+++ public/js/pages/posts.js
@@ -1,5 +1,5 @@
-import {getSinglePost, renderBreadcrumb} from "../function/posts.js";
-import {calculateTimePassed} from "../function/utils.js";
+import {getNote, getSinglePost, renderBreadcrumb} from "../function/posts.js";
+import {baseUrl, calculateTimePassed, getFromSearchParam, getToken} from "../function/utils.js";
 import {renderSwalCallInfo} from "../function/shared.js";
 import {isLogin, loginModal} from "../function/auth.js";
 
@@ -17,77 +17,136 @@ window.addEventListener("load", async () => {
     const callInfoBtn = document.querySelector("#callInfo");
     const trashIcon = document.querySelector("#trashIcon");
 
-    const singlePost = await getSinglePost();
+    const postId = getFromSearchParam("id");
+
+    const singlePost = await getSinglePost(postId);
     const post = singlePost.data.post;
+    let noteId = post?.note._id;
 
+    if (!post) return;
 
-    if (post) {
+    loader.classList.add("hidden");
 
-        loader.classList.add("hidden");
+    const data = calculateTimePassed(post.createdAt);
 
-        const data = calculateTimePassed(post.createdAt);
+    postTitle.innerHTML = `${post.title}`;
+    description.innerHTML = `${post.description}`;
+    postTimeLocation.innerHTML = `${data} در ${post.city.name} . ${post?.neighborhood?.name}`
 
-        postTitle.innerHTML = `${post.title}`;
-        description.innerHTML = `${post.description}`;
-        postTimeLocation.innerHTML = `${data} در ${post.city.name} . ${post?.neighborhood?.name}`
+    //breadcrumb
+    renderBreadcrumb(post.breadcrumbs, post.title, breadcrumbContainer);
 
-        //breadcrumb
-        renderBreadcrumb(post.breadcrumbs, post.title, breadcrumbContainer);
+    //share
+    shareIcon.addEventListener("click", async () => {
+        await navigator.share(location.href);
+    });
 
-        //share
-        shareIcon.addEventListener("click", async () => {
-            await navigator.share(location.href);
-        });
+    //dynamicPostFields
+    post.dynamicFields.forEach((field) => {
 
-        //dynamicPostFields
-        post.dynamicFields.forEach((field) => {
-
-            postFieldContainer.insertAdjacentHTML("beforeend", `
+        postFieldContainer.insertAdjacentHTML("beforeend", `
             <li>
                 <span class="text-sm font-bold">${field.name}</span>
                 <span class="text-primary">${field.data}</span>
             </li>
             `);
 
-        });
+    });
 
-        //call info
-        callInfoBtn.addEventListener("click", () => {
+    //call info
+    callInfoBtn.addEventListener("click", () => {
 
-            if (isLogin()) {
+        if (isLogin()) {
 
-                renderSwalCallInfo();
+            renderSwalCallInfo();
 
-            } else {
+        } else {
 
-                loginModal();
-            }
-        });
+            loginModal();
+        }
+    });
 
-        // note
-        noteTextArea.addEventListener("click", () => {
+    // note
+    noteTextArea.addEventListener("click", () => {
 
-            if (isLogin()) {
+        if (isLogin()) {
 
-                noteTextArea.addEventListener("keyup", () => {
+            // handle trash
+            noteTextArea.addEventListener("keyup", () => {
 
-                    if (noteTextArea.value.trim().length) {
+                if (noteTextArea.value.trim().length) {
 
-                        trashIcon.classList.remove("hidden");
+                    trashIcon.classList.remove("hidden");
 
-                    } else {
+                } else {
 
-                        trashIcon.classList.add("hidden");
-                    }
-                });
+                    trashIcon.classList.add("hidden");
+                }
+            });
 
-            } else {
+            noteTextArea.addEventListener("blur", async (event) => {
 
-                loginModal();
-            }
-        });
+                if (noteId) {
 
+                    const response = await fetch(`${baseUrl}/v1/note/${noteId}`, {
+
+                        method: "PUT", headers: {
+                            "Content-Type": "application/json", Authorization: `Bearer ${getToken()}`
+
+                        }, body: JSON.stringify({
+
+                            content: noteTextArea.value.trim(),
+                        })
+                    });
+
+                } else {
+
+                    if (noteTextArea.value.trim() === "") return;
+                    const response = await fetch(`${baseUrl}/v1/note/`, {
+
+                        method: "POST", headers: {
+                            "Content-Type": "application/json", Authorization: `Bearer ${getToken()}`
+
+                        }, body: JSON.stringify({
+
+                            postId: postId, content: noteTextArea.value.trim(),
+                        })
+                    });
+                    noteId = (await response.json()).data.note._id;
+                }
+            });
+
+        } else {
+
+            loginModal();
+        }
+    });
+
+    // show note if exist
+    if (post.note.content) {
+
+        noteTextArea.innerHTML = post.note.content
+        trashIcon.classList.remove("hidden");
     }
+
+    //delete note
+    trashIcon.addEventListener("mousedown", async (event) => {
+
+        event.preventDefault();
+        if (noteId) {
+
+            const response = await fetch(`${baseUrl}/v1/note/${noteId}`, {
+
+                method: "DELETE", headers: {
+                    Authorization: `Bearer ${getToken()}`
+                },
+            });
+            noteId = null;
+        }
+
+        noteTextArea.value = "";
+        trashIcon.classList.add("hidden");
+    });
 });
 
 
